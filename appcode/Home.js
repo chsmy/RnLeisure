f851fa7555599dfad06c162c24939fec7a9e6076
caoweiaaa <13161083183@163.com>
2016-11-21 17:37:11 +0800

    Signed-off-by: caoweiaaa <13161083183@163.com>

--- appcode/Home.js
+++ appcode/Home.js
@@ -17,77 +17,18 @@ import {
   RefreshControl
 } from 'react-native';
 import MainPage from './MainPage'
-export default class Home extends Component {
-  constructor(props) {
-    super(props)
-    this.pageIndex = 0;
-    this.dataArry = this.props.dataArry;
-    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
-      //  dataSource: ds.cloneWithRows([
-      //   'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
-      // ]),
-      dataArry: [],
-      loadMore: false,
-      isRefreshing: false,
-      isError: false
-    }
-  }
-  componentDidMount () {
-    this.getMoviesFromApiAsync();
-  }
-      getMoviesFromApiAsync() {
+import Animation from './Animation'
 
-     fetch('http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/1')
-      .then((response) => response.json())
-      .then((responseJson) => {
-        this.setState({dataArry : responseJson.results,
-        });
-        console.log(responseJson);
-      })
-      .catch((error) => {
-        console.error(error);
-      });
-  };
+export default class Home extends Component {
+
 
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>首页！</Text>
-        <ListView
-         dataSource={this.state.dataSource.cloneWithRows(this.state.dataArry)}
-          // renderRow={(rowData) => <Text>{rowData}</Text>}
-          // dataSource={this.state.dataSource}
-          renderRow={this._renderItem.bind(this)}
-          enableEmptySections={true}
-          // onEndReached={this._loadmore.bind(this)}
-          // renderFooter={this._renderFooter.bind(this)}
-          // onEndReachedThreshold={29}
-          // refreshControl={
-          //   <RefreshControl
-          //     refreshing={this.state.isRefreshing}
-          //     onRefresh={this._refresh.bind(this)}
-          //     tintColor='#aaaaaa'
-          //     title='Loading...'
-          //     progressBackgroundColor='#aaaaaa' />
-        // } 
-        />
+
       </View>
     );
-  }
-   _renderItem (results) {
-    return (
-      <TouchableHighlight onPress= {() => this._skipIntoContent(contentData)
-      }>
-        <View style={styles.itemContainer}>
-          <Text style={styles.date}>{results.type}</Text>
-          <Text style={[styles.title]}>{results.publishedAt}</Text>
-          <Image source={{uri:results.url}}
-            style={styles.thumbnail}/>
-        </View>
-      </TouchableHighlight>
-    )
   }
 
 }
@@ -99,30 +40,12 @@ const styles = StyleSheet.create({
     // alignItems: 'center',
     backgroundColor: '#F5FCFF',
   },
+
   welcome: {
     fontSize: 20,
     textAlign: 'center',
   },
- thumbnail: {
-    width: null, // 配合alignSelf实现宽度上 match_parent
-    height: 260,
-    alignSelf: 'stretch'
-  },
-  title: {// alignSelf 默认是center
-    fontSize: 15,
-    marginBottom: 10,
-    marginRight: 35,
-    marginLeft: 35,
-    // letterSpacing: 10,//字间距
-    lineHeight: 22, // 行距＋字高，0表示和字高一样，没效果
-    color: 'black',
-    textAlign: 'center' // 字的对其方式：center每行都居中；left，right；auto ＝＝＝ justify ＝＝＝ left
-  },
-  date: {
-    fontSize: 17,
-    color: 'black',
-    textAlign: 'center'
-  },
+
 });
 module.exports = Home
 
--- appcode/Picture.js
+++ appcode/Picture.js
@@ -13,17 +13,113 @@ import {
   TextInput,
   Image,
   TouchableHighlight,
+  ListView,
+  RefreshControl
 } from 'react-native';
 import MainPage from './MainPage'
+import Animation from './Animation'
+let SERVER_URI = "http://gank.io/api/data/%E7%A6%8F%E5%88%A9/10/";
 export default class Picture extends Component {
-  
+  constructor(props) {
+    super(props)
+    this.pageIndex = 1;
+    this.dataArry = this.props.dataArry;
+    const ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+    this.state = {
+      dataSource: new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 }),
+      //  dataSource: ds.cloneWithRows([
+      //   'John', 'Joel', 'James', 'Jimmy', 'Jackson', 'Jillian', 'Julie', 'Devin'
+      // ]),
+      dataArry: [],
+      loadMore: false,
+      isRefreshing: false,
+      isError: false
+    }
+  }
+  componentDidMount() {
+    this.getFromApiAsyncRefresh();
+  }
+  //刷新的时候调用 pageIndex置一
+  getFromApiAsyncRefresh() {
+    this.pageIndex = 1
+    fetch(SERVER_URI + this.pageIndex)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataArry: responseJson.results,
+          loadMore: false,
+        });
+        console.log(responseJson);
+        this.pageIndex++;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+  //loadMore 的时候调用
+  getListFromApiAsync() {
+    this.setState({ loadMore: true })
+    fetch(SERVER_URI + this.pageIndex)
+      .then((response) => response.json())
+      .then((responseJson) => {
+        this.setState({
+          dataArry: this.state.dataArry.concat(responseJson.results),
+          loadMore: false,
+        });
+        console.log(responseJson);
+        this.pageIndex++;
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
+
   render() {
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>Picture！</Text>
-        
+        <ListView
+          dataSource={this.state.dataSource.cloneWithRows(this.state.dataArry)}
+          // renderRow={(rowData) => <Text>{rowData}</Text>}
+          // dataSource={this.state.dataSource}
+          renderRow={this._renderItem.bind(this)}
+          enableEmptySections={true}
+          onEndReached={this.getListFromApiAsync.bind(this)}
+          renderFooter={this._renderFooter.bind(this)}
+          onEndReachedThreshold={29}
+          refreshControl={
+            <RefreshControl
+              refreshing={this.state.isRefreshing}
+              onRefresh={this.getFromApiAsyncRefresh.bind(this)}
+              tintColor='#aaaaaa'
+              title='Loading...'
+              progressBackgroundColor='#aaaaaa' />
+          }
+          />
       </View>
     );
+  }
+  _renderItem(results) {
+    return (
+      <TouchableHighlight onPress={() => this._skipIntoContent(contentData)
+      }>
+        <View style={styles.itemContainer}>
+          <Image source={{ uri: results.url }}
+            style={styles.thumbnail} />
+          <Text style={styles.date}>{results.type}</Text>
+          <Text style={[styles.title]}>{results.publishedAt}</Text>
+        </View>
+      </TouchableHighlight>
+    )
+  }
+  _renderFooter() {
+    return (
+      this.state.loadMore
+        ? (<View style={[styles.indicatorWrapper]}>
+          <Animation timingLength={50} duration={500} bodyColor={'#aaaaaa'} />
+        </View>)
+        : null
+    )
   }
 }
 
@@ -37,14 +133,46 @@ const styles = StyleSheet.create({
   welcome: {
     fontSize: 20,
     textAlign: 'center',
-    marginTop: 100,
   },
- 
+
   header: {
     height: 70,
     width: 70,
 
-  }
+  },
+  itemContainer: {
+    flexDirection: 'column',
+    // height: 30,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingTop: 20
+  },
+  thumbnail: {
+    width: null, // 配合alignSelf实现宽度上 match_parent
+    height: 260,
+    alignSelf: 'stretch'
+  },
+  title: {// alignSelf 默认是center
+    fontSize: 15,
+    marginBottom: 10,
+    marginRight: 35,
+    marginLeft: 35,
+    // letterSpacing: 10,//字间距
+    lineHeight: 22, // 行距＋字高，0表示和字高一样，没效果
+    color: 'black',
+    textAlign: 'center' // 字的对其方式：center每行都居中；left，right；auto ＝＝＝ justify ＝＝＝ left
+  },
+  date: {
+    fontSize: 17,
+    color: 'black',
+    textAlign: 'center'
+  },
+  indicatorWrapper: {
+    height: 45,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#252528'
+  },
 });
 module.exports = Picture
 // AppRegistry.registerComponent('Leisure', () => Leisure);
